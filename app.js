2b691308874395d20cb7df094bb8ed6390a18502
subashbasnyat <subash.basnet0123@gmail.com>
2017-08-09 21:51:22 +0545

    mlab and port
    Mlab and Port Env Ip

--- app.js
+++ app.js
@@ -9,8 +9,8 @@ var flash = require('connect-flash');
 var fs = require('fs');
 var multer = require('multer');
 mongoose.Promise = global.Promise;
-mongoose.connect("mongodb://localhost/campdb");
-// mongoose.connect("mongodb://subashbasnyat:subashbasnet0123@ds155841.mlab.com:55841/lplaces");
+// mongoose.connect("mongodb://localhost/campdb");
+mongoose.connect("mongodb://subashbasnyat:subashbasnet0123@ds155841.mlab.com:55841/lplaces");
 
 var seedDB = require("./seeds");
 
@@ -67,10 +67,10 @@ app.use("/places/:id/comments",commentRoutes);
 //AUTH ROUTES
 //=============================
 
-// app.listen(process.env.PORT,process.env.IP,function(){
-// 	console.log("GOOD to GO");
-// });
+app.listen(process.env.PORT,process.env.IP,function(){
+	console.log("GOOD to GO");
+});
 
-app.listen(3000,function(){
-	console.log("Good to Go");
-})
+// app.listen(3000,function(){
+// 	console.log("Good to Go");
+// })
